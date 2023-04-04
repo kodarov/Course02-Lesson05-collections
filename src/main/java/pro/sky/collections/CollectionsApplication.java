6b382eac17b5d6bb07b064e18c54b735073c95aa
kodarov_salavat <kodarov@gmail.com>
2023-04-05 00:06:09 +0400

    1st

--- src/main/java/pro/sky/collections/CollectionsApplication.java
+++ src/main/java/pro/sky/collections/CollectionsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CollectionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CollectionsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsApplication.class, args);
+    }
 
 }
